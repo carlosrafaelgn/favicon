225eba39fcb55774424dad7441c13fe7189f019c
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2021-04-30 18:10:07 -0300

    Adding new meta tags

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@
 // whenever it detects a change in the source code of the
 // service worker).
 const CACHE_PREFIX = "favicon-helper-static-cache";
-const CACHE_VERSION = "-v4";
+const CACHE_VERSION = "-20210430";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 
 self.addEventListener("install", (event) => {
@@ -67,7 +67,6 @@ self.addEventListener("install", (event) => {
 		// cache.addAll() to this.
 		const files = [
 			"/favicon/",
-			"/favicon/?pwa",
 			"/favicon/favicon.ico",
 			"/favicon/favicon.png",
 			"/favicon/favicons/favicon-512x512.png",
